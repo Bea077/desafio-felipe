898a4bffe810ce4a95fff6c630748051a7d2fd80
Bea077 <biabari123@gmail.com>
2024-01-19 16:50:03 -0400

    primeira versão desafio

---
+++ src/index.js
@@ -0,0 +1,41 @@
+let heroName = "Scorpion"
+
+let heroLevel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+let xpHero = 1000 
+
+
+while (xpHero < 1000){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[0]}`)
+}    
+    
+ while (xpHero >= 1000 && xpHero < 2000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[1]}`)
+}
+
+while (xpHero >= 2000 && xpHero < 5000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[2]}`)
+}
+
+while (xpHero >= 5000 && xpHero < 7000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[3]}`)
+}
+
+while (xpHero >= 7000 && xpHero < 8000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[4]}`)
+}
+    
+while (xpHero >= 8000 && xpHero < 9000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[5]}`)
+}
+
+while (xpHero >= 9000 && xpHero <= 10000 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[6]}`)
+}
+
+while (heroXp >=10001 ){
+    console.log(`O herói de nome: ${heroName}; está no nível: ${heroLevel[7]}`)
+}
+    
+    
+    
